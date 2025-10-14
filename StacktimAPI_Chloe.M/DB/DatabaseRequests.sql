aa86d2be21918c202179e0b4a46012c15492301b
Chloé <c.martinez@xefi.fr>
2025-10-14 23:52:03 +0200

    create team-composition request

--- StacktimAPI_Chloe.M/DB/DatabaseRequests.sql
+++ StacktimAPI_Chloe.M/DB/DatabaseRequests.sql
@@ -1,2 +1,7 @@
 -- leaderboard
 SELECT Pseudo, TotalScore FROM Players ORDER BY TotalScore DESC;
+
+-- team composition
+SELECT Teams.Name, Players.Pseudo, TeamPlayers.Role FROM Teams 
+JOIN TeamPlayers ON Teams.Id = TeamPlayers.TeamId
+JOIN Players ON Players.Id = TeamPlayers.PlayerId;
