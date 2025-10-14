2cc5d86370575b2baa05309fd5b6bcf659ef205f
Chloé <c.martinez@xefi.fr>
2025-10-15 00:00:46 +0200

    create team-stats request

--- StacktimAPI_Chloe.M/DB/DatabaseRequests.sql
+++ StacktimAPI_Chloe.M/DB/DatabaseRequests.sql
@@ -5,3 +5,9 @@ SELECT Pseudo, TotalScore FROM Players ORDER BY TotalScore DESC;
 SELECT Teams.Name, Players.Pseudo, TeamPlayers.Role FROM Teams 
 JOIN TeamPlayers ON Teams.Id = TeamPlayers.TeamId
 JOIN Players ON Players.Id = TeamPlayers.PlayerId;
+
+-- team stats 
+SELECT Teams.Name, COUNT(TeamPlayers.PlayerId) AS "Players count", AVG(Players.TotalScore) AS "Average score" FROM Teams
+JOIN TeamPlayers ON Teams.Id = TeamPlayers.TeamId
+JOIN Players ON Players.Id = TeamPlayers.PlayerId
+GROUP BY Teams.Name;
