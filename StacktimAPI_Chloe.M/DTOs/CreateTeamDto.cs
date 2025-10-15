f453fad3f497cebb2b394381970e97d44d8a3ef7
Chloé <c.martinez@xefi.fr>
2025-10-16 01:16:12 +0200

    create CreateTeamDto

---
+++ StacktimAPI_Chloe.M/DTOs/CreateTeamDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StacktimAPI_Chloe.DTOs
+{
+    public class CreateTeamDto
+    {
+        [Required, StringLength(100, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required, StringLength(3, MinimumLength = 3), RegularExpression("[A-Z][A-Z][A-Z]")]
+        public string Tag { get; set; }
+
+        [Required]
+        public int CaptainId { get; set; }
+
+    }
+}
