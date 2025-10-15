0ddc39e5c8014472fa59de731c46fa06fb793f57
Chloé <c.martinez@xefi.fr>
2025-10-15 21:32:27 +0200

    create PlayerDto

---
+++ StacktimAPI_Chloe.M/DTOs/PlayerDto.cs
@@ -0,0 +1,11 @@
+﻿namespace StacktimAPI_Chloe.M.DTOs
+{
+    public class PlayerDto
+    {
+        public int Id { get; set; }
+        public string Pseudo { get; set; }
+        public string Email { get; set; }
+        public string Rank { get; set; }
+        public int TotalScore { get; set; }
+    }
+}
