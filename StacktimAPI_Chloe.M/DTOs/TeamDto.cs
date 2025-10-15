f7080fa2bc5314d1a6faffdcc3efb0612b7db87e
Chloé <c.martinez@xefi.fr>
2025-10-16 00:47:47 +0200

    create TeamDto

---
+++ StacktimAPI_Chloe.M/DTOs/TeamDto.cs
@@ -0,0 +1,10 @@
+﻿namespace StacktimAPI_Chloe.DTOs
+{
+    public class TeamDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Tag { get; set; }
+        public int CaptainId { get; set; }
+    }
+}
