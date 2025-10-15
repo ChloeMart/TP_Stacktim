b6a8e1d29d2a185cae069bb6a8b2d9a7f88b05e2
Chloé <c.martinez@xefi.fr>
2025-10-15 21:36:33 +0200

    create UpdatePlayerDto

---
+++ StacktimAPI_Chloe.M/DTOs/UpdatePlayerDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StacktimAPI_Chloe.M.DTOs
+{
+    public class UpdatePlayerDto
+    {
+        [StringLength(50, MinimumLength = 3)]
+        public string? Pseudo { get; set; }
+
+        [EmailAddress, StringLength(100)]
+        public string? Email { get; set; }
+
+        [RegularExpression("Bronze|Silver|Gold|Platinum|Diamond|Master")]
+        public string? Rank { get; set; }
+
+        public int? TotalScore { get; set; }
+    }
+}
