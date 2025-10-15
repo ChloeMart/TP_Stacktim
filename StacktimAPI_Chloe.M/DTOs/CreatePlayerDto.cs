2ccce31a04daa773580ae9b233c55fda047e191e
Chloé <c.martinez@xefi.fr>
2025-10-15 21:34:35 +0200

    create CreatePlayerDto

---
+++ StacktimAPI_Chloe.M/DTOs/CreatePlayerDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StacktimAPI_Chloe.M.DTOs
+{
+    public class CreatePlayerDto
+    {
+        [Required, StringLength(50, MinimumLength = 3)]
+        public string Pseudo { get; set; }
+
+        [Required, EmailAddress, StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression("Bronze|Silver|Gold|Platinum|Diamond|Master")]
+        public string Rank { get; set; }
+    }
+}
