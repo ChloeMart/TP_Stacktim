2d521189b13ef264760786ca8fb62139f57533e1
Chloé <c.martinez@xefi.fr>
2025-10-16 01:22:35 +0200

    create POST /api/teams

--- StacktimAPI_Chloe.M/Controllers/TeamsController.cs
+++ StacktimAPI_Chloe.M/Controllers/TeamsController.cs
@@ -69,5 +69,34 @@ namespace StacktimAPI_Chloe.Controllers
             }
         }
 
+        [HttpPost]
+        public IActionResult CreateTeam([FromBody] CreateTeamDto dto)
+        {
+            if (_context.Teams.Any(t => t.Name == dto.Name || t.Tag == dto.Tag))
+            {
+                return BadRequest(new { message = "Name or tag already used" });
+            }
+
+            var team = new Team
+            {
+                Name = dto.Name,
+                Tag = dto.Tag,
+                CaptainId = dto.CaptainId,
+            };
+
+            _context.Teams.Add(team);
+            _context.SaveChanges();
+
+            var teamDto = new TeamDto
+            {
+                Id = team.Id,
+                Name = team.Name,
+                Tag = team.Tag,
+                CaptainId = team.CaptainId
+            };
+
+            return CreatedAtAction(nameof(GetTeam), new { id = team.Id }, teamDto);
+        }
+
     }
 }
