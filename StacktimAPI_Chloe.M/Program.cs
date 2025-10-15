81486fa01291a5b00ad5facbd02ba1116fc4457a
Chloé <c.martinez@xefi.fr>
2025-10-15 21:22:58 +0200

    configuration

--- StacktimAPI_Chloe.M/Program.cs
+++ StacktimAPI_Chloe.M/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using StacktimAPI_Chloe.M.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<StacktimDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
